9a5ef22a6d6c92f647b053ac08dca5480a9e44f2
Vlad <131590167+VladimirCheshmedzhiev@users.noreply.github.com>
2023-07-06 17:29:23 +0300

    Add files via upload

---
+++ 4.LastKNumbersSequence.js
@@ -0,0 +1,16 @@
+function lastNumbers(length, k){
+
+let arr = [1];
+
+while(length > arr.length){
+    let kArr = arr.slice(-k);
+    let sum = 0;
+    for (let i = 0; i < kArr.length; i++) {
+        sum += kArr[i];
+    }
+    arr.push(sum);
+}
+    console.log(arr.join(' '))
+
+}
+lastNumbers(6, 3)
